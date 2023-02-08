8d6e2bfc1290cec24f4b6ebccdd691c263cf5c05
Anton <>
2023-02-08 10:24:51 +0100

    Add project files.

---
+++ Uppgift 3.12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur många datorer äger du? : ");
+            int datorer = Convert.ToInt32(Console.ReadLine());
+
+            if (datorer == 1)
+                Console.WriteLine("Du har 1 dator.");
+
+            else if (datorer > 1)
+                Console.WriteLine("Du har " + datorer + " datorer.");
+
+            else if (datorer <= 0)
+                Console.WriteLine(" :( ");
+               
+
+
+        }
+    }
+}
